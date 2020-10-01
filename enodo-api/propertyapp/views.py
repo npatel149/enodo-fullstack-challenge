3e0ab808c354c388dbff6bf82af88cb91b69641a
Nitin <er.nitinpatel914@gmail.com>
2020-10-01 10:12:16 -0500

    REST - Add search and ordering features.

--- enodo-api/propertyapp/views.py
+++ enodo-api/propertyapp/views.py
@@ -1,11 +1,21 @@
 from .models import PropertyInfo
 from .serializers import PropertyInfoSerializer
 from rest_framework import permissions, generics
+from rest_framework.filters import SearchFilter, OrderingFilter
 
 
 class PropertyInfoList(generics.ListCreateAPIView):
     queryset = PropertyInfo.objects.all()
     serializer_class = PropertyInfoSerializer
+    filter_backends = [SearchFilter, OrderingFilter]
+    search_fields = ['full_address',
+                     'zip',
+                     'rec_type',
+                     'class_description',
+                     'pprior_year',
+                     'town', 'city',
+                     'res_type',
+                     'bldg_use', ]
 
 
 class PropertyInfoDetail(generics.RetrieveUpdateAPIView):
