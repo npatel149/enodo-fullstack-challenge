e6a12fa5644eb5bd0638960542c341c23a67ba25
Nitin <er.nitinpatel914@gmail.com>
2020-10-01 10:47:08 -0500

    UI - Add element-ui for styling.

--- enodo-app/src/main.js
+++ enodo-app/src/main.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
+import ElementUI from 'element-ui';
+// import 'element-ui/lib/theme-chalk/index.css';
+import './styles/element-variables.scss'
 import App from './App.vue'
 import router from './router'
+
+Vue.use(ElementUI);
 
 Vue.config.productionTip = false
 
