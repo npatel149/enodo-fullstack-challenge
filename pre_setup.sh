70cb42b045cb1ff36780a20b2985d140f25070bc
Nitin <er.nitinpatel914@gmail.com>
2020-10-01 11:25:54 -0500

    PRESET - Add django manage migrate command.

--- pre_setup.sh
+++ pre_setup.sh
@@ -7,9 +7,9 @@ GUI_BASE="./enodo-app"
 API_BASE="./enodo-api"
 
 # Dependencies
-# node 8.12.0
-# yarn 1.7.0
-# Python 3.6.6
+# node 12.17.0
+# npm 6.14.4
+# Python 3.8.0
 
 node -v
 npm -v
@@ -26,6 +26,7 @@ python3 -m venv .venv
 . .venv/bin/activate
 pip3 install -r requirements_prod.txt
 python3 presetup_load_xlsx_data_to_sqlite_db.py
+python3 manage.py migrate
 popd
 
 echo "Done!"
