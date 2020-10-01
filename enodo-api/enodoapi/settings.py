e0c34687eff1a131d7d0b3fcde612ba3636a29e5
Nitin <er.nitinpatel914@gmail.com>
2020-10-01 11:13:06 -0500

    REST - Add CORS settings to allow requests from Frontend/axios.

--- enodo-api/enodoapi/settings.py
+++ enodo-api/enodoapi/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    # CORS
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -51,6 +53,13 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:8081',
+)
+# CORS Config
+CORS_ALLOW_CREDENTIALS = False
 
 ROOT_URLCONF = 'enodoapi.urls'
 
